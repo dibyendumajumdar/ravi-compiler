7432f737e82006a7dc9d9829b64395d43386cfcc
Dibyendu Majumdar <mobile@majumdar.org.uk>
2020-07-12 21:41:08 +0100

    issue #24 Initial implementation of data flow framework

---
+++ src/dataflow_framework.c
@@ -0,0 +1,67 @@
+/**
+ * A framework fpor performing data flow analysis.
+ * The framework is based upon similar framework in MIR project.
+ */
+
+#include "dataflow_framework.h"
+#include "graph.h"
+
+#include <string.h>
+
+void raviX_init_data_flow(struct dataflow_context *dataflow_context, struct graph *g)
+{
+	memset(dataflow_context, 0, sizeof *dataflow_context);
+	raviX_bitset_create2(&dataflow_context->bb_to_consider, 512);
+	dataflow_context->g = g;
+}
+
+void raviX_finish_data_flow(struct dataflow_context *dataflow_context)
+{
+	array_clearmem(&dataflow_context->worklist);
+	array_clearmem(&dataflow_context->pending);
+	raviX_bitset_destroy(&dataflow_context->bb_to_consider);
+}
+
+void raviX_solve_dataflow(struct dataflow_context *dataflow_context, bool forward_p,
+			  int (*join_function)(void *, nodeId_t, bool),
+			  int (*transfer_function)(void *, nodeId_t))
+{
+	unsigned iter;
+	struct node_array *worklist = &dataflow_context->worklist;
+	struct node_array *pending = &dataflow_context->pending;
+
+	worklist->count = 0;
+	for (uint32_t i = 0; i < raviX_graph_size(dataflow_context->g); i++) {
+		array_push(worklist, raviX_graph_node(dataflow_context->g, i));
+	}
+	iter = 0;
+	while (worklist->count != 0) {
+		struct node_array *t = NULL;
+		struct node **addr = worklist->data;
+		raviX_sort_nodes_by_RPO(addr, worklist->count, forward_p);
+		raviX_bitset_clear(&dataflow_context->bb_to_consider);
+		pending->count = 0;
+		for (unsigned i = 0; i < worklist->count; i++) {
+			int changed_p = iter == 0;
+			struct node *bb = addr[i];
+			struct node_list *nodes = forward_p ? raviX_predecessors(bb) : raviX_successors(bb);
+			if (raviX_node_list_size(nodes) == 0)
+				join_function(dataflow_context->userdata, raviX_node_index(bb), true);
+			else
+				changed_p |= join_function(dataflow_context->userdata, raviX_node_index(bb), false);
+			if (changed_p && transfer_function(dataflow_context->userdata, raviX_node_index(bb))) {
+				struct node_list *list = forward_p ? raviX_successors(bb) : raviX_predecessors(bb);
+				for (unsigned i = 0; i < raviX_node_list_size(list); i++) {
+					nodeId_t index = raviX_node_list_at(list, i);
+					if (raviX_bitset_set_bit_p(&dataflow_context->bb_to_consider, index)) {
+						array_push(pending, raviX_graph_node(dataflow_context->g, index));
+					}
+				}
+			}
+		}
+		iter++;
+		t = worklist;
+		worklist = pending;
+		pending = t;
+	}
+}
---
+++ src/dataflow_framework.h
@@ -0,0 +1,27 @@
+#ifndef ravicomp_DATAFLOW_FRAMEWORK_H
+#define ravicomp_DATAFLOW_FRAMEWORK_H
+
+#include "allocate.h"
+#include "graph.h"
+#include "bitset.h"
+
+#include <stdbool.h>
+
+DECLARE_ARRAY(node_array, struct node *);
+
+struct dataflow_context {
+	struct graph *g;
+	struct node_array worklist;
+	struct node_array pending;
+	struct bitset_t bb_to_consider;
+	void *userdata;
+};
+
+extern void raviX_init_data_flow(struct dataflow_context *dataflow_context, struct graph *g);
+extern void raviX_finish_data_flow(struct dataflow_context *dataflow_context);
+extern void raviX_solve_dataflow(struct dataflow_context *dataflow_context, bool forward_p,
+				 int (*join_function)(void *, nodeId_t, bool init),
+				 int (*transfer_function)(void *, nodeId_t));
+
+
+#endif
