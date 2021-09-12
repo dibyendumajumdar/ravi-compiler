29fff31c6b04b06f9cb82c88a8c62d219314b81d
dibyendumajumdar <mobile@majumdar.org.uk>
2021-09-12 14:38:56 +0100

    #74 simple test

--- tests/tchibicc.c
+++ tests/tchibicc.c
@@ -2,18 +2,34 @@
 
 #include <string.h>
 
+static void printout(char *key, int keylen, void *val)
+{
+	printf("name: %d %.*s\n", keylen, keylen, key);
+}
+
 int main(int argc, const char *argv[])
 {
 	char buffer[100] = {0};
+	char buffer2[100] = {0};
 	const char *code = "" \
- 	"int main() {\n" \
-	" return 0;\n" \
-	"}\n";
+ 	"typedef struct {\n" \
+	" char *data;\n" \
+	" int len;\n" \
+	"} Str;\n";
 	strncpy(buffer, code, sizeof buffer);
 	C_parser parser = {0};
 	Token *tok = tokenize_buffer(&parser, buffer);
 	convert_pp_tokens(&parser, tok);
 	Scope scope = {0};
 	Obj *obj = parse(&scope, &parser, tok);
+	hashmap_foreach(&scope.vars, printout);
+	create_function(&scope, &parser, "dummy");
+	hashmap_foreach(&scope.vars, printout);
+	const char *snippet = "{ Str s; s.data = \"hello world\"; s.len = sizeof \"hello world\"; }\n";
+	strncpy(buffer2, snippet, sizeof buffer2);
+	tok = tokenize_buffer(&parser, buffer2);
+	convert_pp_tokens(&parser, tok);
+	parser.allow_partial_parsing = true;
+	Node *node = parse_compound_statement(&scope, &parser, tok);
 	return 0;
 }
